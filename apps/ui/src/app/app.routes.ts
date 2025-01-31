ccabf48161707b2a73b60016b0f035ec91e82b36
ilyap <ilya.polukhin2002@yandex.ru>
2025-01-31 22:27:24 +0300

    feat: remove profile page from routes

--- apps/ui/src/app/app.routes.ts
+++ apps/ui/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Route } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { ShortLinkRedirectComponent } from './short-link-redirect/short-link-redirect.component';
 import { LoginComponent } from './login-page/login-page.component';
 import { RegisterComponent } from './register-page/register-page.component';
@@ -10,7 +9,7 @@ import { AuthGuard } from './auth/auth.guard';
 
 export const appRoutes: Route[] = [
   { path: '', component: MainPageComponent, pathMatch: 'full', canActivate: [AuthGuard] },
-  { path: 'profile', component: ProfilePageComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+  // { path: 'profile', component: ProfilePageComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'signup', component: RegisterComponent, pathMatch: 'full' },
   { path: 'create', component: CreateShortLinkPageComponent, pathMatch: 'full', canActivate: [AuthGuard] },
